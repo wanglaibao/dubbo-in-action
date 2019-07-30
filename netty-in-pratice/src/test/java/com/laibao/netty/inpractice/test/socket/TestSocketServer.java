79c87d1633eb7b084380995f51e3bf0eb3574f21
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-07-30 13:32:10 +0800

    update pom.xml file info

---
+++ netty-in-pratice/src/test/java/com/laibao/netty/inpractice/test/socket/TestSocketServer.java
@@ -0,0 +1,30 @@
+package com.laibao.netty.inpractice.test.socket;
+
+import com.laibao.netty.inpractice.test.httpserver.TestServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TestSocketServer {
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup bossGroup = new NioEventLoopGroup();  //用来接收请求的
+        EventLoopGroup workerGroup = new NioEventLoopGroup(); //用来处理任务的
+
+        try{
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)  //通过反射的方式来进行创建的
+                    .childHandler(new TestSocketServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            bossGroup.shutdownGracefully().sync();
+            workerGroup.shutdownGracefully().sync();
+        }
+    }
+}
---
+++ netty-in-pratice/src/test/java/com/laibao/netty/inpractice/test/socket/TestSocketServerInitializer.java
@@ -0,0 +1,25 @@
+package com.laibao.netty.inpractice.test.socket;
+
+import com.laibao.netty.inpractice.test.httpserver.TestHttpServerHandler;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.CharsetUtil;
+
+public class TestSocketServerInitializer extends ChannelInitializer<SocketChannel>{
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+        pipeline.addLast("lengthFieldBasedFrameDecoder",new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE,0,4,0,4));
+        pipeline.addLast("lengthFieldPrepender",new LengthFieldPrepender(4));
+        pipeline.addLast("stringDecoder",new StringDecoder(CharsetUtil.UTF_8));
+        pipeline.addLast("stringEncoder",new StringEncoder(CharsetUtil.UTF_8));
+        pipeline.addLast(null);
+    }
+}
