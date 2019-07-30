dd9487323e483ae8e7a320aa6073c54e882231a0
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-07-30 11:39:03 +0800

    update pom.xml file info

--- netty-in-pratice/src/test/java/com/laibao/netty/inpractice/test/TestHttpServerHandler.java
+++ netty-in-pratice/src/test/java/com/laibao/netty/inpractice/test/TestHttpServerHandler.java
@@ -7,18 +7,69 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject>{
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
         if (msg instanceof HttpRequest) {
+            HttpRequest request = (HttpRequest)msg;
+            System.out.println("请求的方法名称是: "+request.method().name());
+
+            URI uri = new URI(request.uri());
+            if ("/".equalsIgnoreCase(uri.getPath())){
+                System.out.println("请求favicon.ico");
+                return;
+            }
+
             ByteBuf context = Unpooled.copiedBuffer("Hello,World", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
                     HttpResponseStatus.OK,
                     context);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
             response.headers().set(HttpHeaderNames.CONTENT_LENGTH,context.readableBytes());
+
+            System.out.println("我被执行了! 444");
             ctx.writeAndFlush(response);
         }
     }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerAdded    111");
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelRegistered  222");
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelActive 333");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelInactive  555");
+    }
+
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channelUnregistered  666");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handlerRemoved  777");
+    }
+
+
 }
